2f7f95c037ab47a59b4fb7cf02bf8f3008b17813
runklem <runklem@gmail.com>
2015-08-29 15:54:07 -0500

    hjhghj

---
+++ replykeys.py
@@ -0,0 +1,10 @@
+
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+consumer_key =          'JyJzut7NCDnwoLnrJHjopMjeq',
+consumer_secret =       'qtyC0syWGHqE4TiahiZoZyX8QW6AEbjZzchVomvZMmjdSGJDRV', 
+
+
+access_token =          '3305724977-BkrBc5YEqjtgDVod306MKAmmF1zBXrSL3GB0fXI',
+access_token_secret =   'qULxQbFvDO3NOP1ibogyhcBdqpuAjLDMdAJeHbcZPik1k',
+)
