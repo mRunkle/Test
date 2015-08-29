3e1d7a7b91fb71016047ee7d816ce2772b2c49d9
runklem <runklem@gmail.com>
2015-08-29 14:03:19 -0500

    added linked list

---
+++ Linked List/Linked List/List.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    public class List
+    {
+        public Node headNode;
+
+        public List()
+        {
+            headNode = null;
+        }
+
+        public void addToEnd(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                addToBeginning(data);
+            }
+        }
+
+        public void addSorted(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else if (data < headNode.data)
+            {
+                Node temp = new Node(data);
+                temp.next = headNode;
+                headNode = temp;
+            }
+            else
+            {
+                headNode.addSorted(data);
+            }
+        }
+
+        public void addToBeginning(int data)
+        {
+            if (headNode == null)
+            {
+                headNode = new Node(data);
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = headNode;
+                headNode = temp;
+            }
+        }
+
+        public void print()
+        {
+            if (headNode != null)
+            {
+                headNode.print();
+            }
+        }
+    }
+}
---
+++ Linked List/Linked List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            //list.addToEnd(9);
+            //list.addToEnd(5);
+            //list.addToEnd(7);
+            //list.addToEnd(11);
+
+            //list.addToBeginning(9);
+            //list.addToBeginning(5);
+            //list.addToBeginning(7);
+            //list.addToBeginning(11);
+
+            //test comments
+
+            list.addSorted(9);
+            list.addSorted(5);
+            list.addSorted(7);
+            list.addSorted(11);
+
+            list.print();
+
+            Console.ReadLine();
+        }
+    }
+}
