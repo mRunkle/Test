b58d7c40f9f4678422042a0422ad2eb9db098502
runklem <runklem@gmail.com>
2015-08-29 15:57:18 -0500

    ertert

---
+++ Customers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Customers
+(
+	CustomerID int NOT NULL PRIMARY KEY,
+	
+	CompanyName varchar(255) NOT NULL,
+	ContactName varchar(255) NULL,
+	ContactTitle varchar(255) NULL,
+	StreetAddress varchar(255) NULL,
+	City varchar(255) NULL,
+	Region varchar(255) NULL,
+	PostalCode varchar(255) NULL,
+	Country varchar(255) NULL,
+	Phone varchar(255) NULL,
+	Fax varchar(255) NULL,
+);
