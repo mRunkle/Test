2282e8545cadf9ec190b1d747d0b86b75963e318
runklem <runklem@gmail.com>
2015-08-29 14:17:59 -0500

    election

---
+++ ElectionPolls/ElectionPolls/ElectionPolls/Candidate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionPolls
+{
+    public class Candidate
+    {
+        protected string lastName;
+        protected List<int> pollRatings;
+
+        public Candidate(string lastName, List<int> ratings)
+        {
+
+        }
+    }
+}
---
+++ ElectionPolls/ElectionPolls/ElectionPolls/Service.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ElectionPolls
+{
+    public class Service
+    {
+        string lastName;
+        List<int> ratings;
+
+        public Candidate createCandidateFromFile(string fileName)
+        {
+            var reader = new StreamReader(File.OpenRead(@"C:\Users\mrunkle_he\Desktop\" + fileName));
+            
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var value = line.Split(',');
+
+                lastName = value[0];
+
+                for (int i = 1; i < value.Length; i++)
+                {
+                    ratings.Add(int.Parse(value[i]));
+                }
+            }
+
+            return new Candidate(lastName, ratings);
+        }
+    }
+}
