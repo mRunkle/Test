b2ed38f5a4fe74ec6a50226d16213abe1aeb8714
runklem <runklem@gmail.com>
2015-08-29 14:13:57 -0500

    added roman conversions

---
+++ RomanConversion/RomanConversion/RomanConversion/InputProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanConversion
+{
+    public class InputProcessor
+    {
+        //MEMBER VARIABLES
+        public int numericalValue;
+        public string romanicalValue;
+        public List<int> numericalDigits = new List<int>();
+        public List<char> romanicalDigits = new List<char>();
+
+        //CONSTRUCTORS
+        public InputProcessor(string romanNumeral)
+        {
+
+        }
+        
+        public InputProcessor(int arabicNumeral)
+        {
+            
+        }
+    }
+}
---
+++ RomanConversion/RomanConversion/RomanConversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CREATE ROMAN-ARABIC NUMERAL PAIRS
+            BaseNumerals baseNumerals = new BaseNumerals();
+            baseNumerals.createRomanBase();
+
+            //STORE BASE NUMERAL COLLECTION IN MAIN() DICTIONARY
+            Dictionary<string, int> numeralCollection = baseNumerals.BaseNumeralsCollection;
+
+
+            foreach (KeyValuePair<string, int> pair in numeralCollection)
+            {
+                Console.WriteLine(pair);
+            }
+
+            Console.WriteLine("Enter either Roman or Arabic numeral for conversion: ");
+            var userInput = Console.ReadLine();
+
+            Console.WriteLine(userInput);
+
+            Console.ReadLine();
+        }
+    }
+}
